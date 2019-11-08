0af397b1d59f4166037e7512797ca9eec0e93435
Victor Simão <victordiogosimao@gmail.com>
2019-11-08 11:52:43 -0300

    Subir Aula 3

--- Aulas/aula3operacoes.py
+++ Aulas/aula3operacoes.py
@@ -1,8 +1,17 @@
 nome = input('Digite seu nome:')
 sobrenome = input('Digite seu sobrenome:')
-idade = int(input('Digite sua idade:'))
+idade = int(input('Digite sua idade:'))fa
 
 print(f'Nome Completo: {nome} {sobrenome}\nIdade: {idade} anos')
 
 if idade < 18:
     print('Menor')
+if idade >= 18:
+    print('Maior')
+if idade == 18:
+    print('o/')
+
+if idade < 18:
+    print('Menor')
+else:
+    print('Maior')
