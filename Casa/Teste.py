072a0f08bec4c2ffa351e20eb11d3442843ecb32
Victor Simão <victordiogosimao@gmail.com>
2019-11-11 20:17:12 -0300

    Estudo em casa

--- Casa/Teste.py
+++ Casa/Teste.py
@@ -1,1 +1,14 @@
-print("Teste")
+#Teste feito em casa
+
+for i in 'Python':
+    print(i)
+
+a, b, c = range(3)
+print(a)
+print(b)
+print(c)
+
+aaa = ['item1', 'item2', 'item3']
+for a, b in enumerate(aaa):
+    print(a)
+    print(b)
