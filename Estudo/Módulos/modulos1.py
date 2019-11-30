7048d5d279bcea18787c9e5279ea129c0f55d584
Victor Simão <victordiogosimao@gmail.com>
2019-11-29 22:17:23 -0300

    Estudo modulo

---
+++ Estudo/Módulos/modulos1.py
@@ -0,0 +1,17 @@
+import math
+
+print(math.sqrt(16))
+
+
+from math import sqrt
+
+print(sqrt(4))
+
+import random
+
+num = random.randint(1, 100)
+print(num)
+
+import emoji
+
+print(emoji.emojize(':sunglasses:'))
