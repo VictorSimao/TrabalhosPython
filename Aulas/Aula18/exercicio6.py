0204c02d0846471a54f8cd20c0d508d3ddf69e22
Victor Simão <victordiogosimao@gmail.com>
2019-12-03 16:29:55 -0300

    Exercicio Aula 18

--- Aulas/Aula18/exercicio6.py
+++ Aulas/Aula18/exercicio6.py
@@ -11,3 +11,59 @@
 
 # 3 - Faça uma terceira função que ao digitar o código do participante ele imprima o nome do participante, 
 # o valor do ingresso, e em caso de menores de idade apareça o texto "Entrada Proibida!"
+
+def dicionario():
+    lista_dic = []
+    arquivo = open('Aulas/Aula18/cadastro.txt','r')
+    for linha in arquivo:
+        linha = linha.strip()
+        lista_linha = linha.split(';')
+        dic_convidados = {'codigo':lista_linha[0],'nome':lista_linha[1],'sexo':lista_linha[2],'idade':lista_linha[3]}
+        lista_dic.append(dic_convidados)
+    arquivo.close()
+    return lista_dic
+
+def separar_sexo():
+    for linha in convidados:
+        if linha['sexo'] == 'f':
+            arquivo = open('Aulas/Aula18/lista_mulher.txt','a')
+            arquivo.write(f"{linha['codigo']};{linha['nome']};{linha['sexo']};{linha['idade']}\n")
+            arquivo.close()
+        elif linha['sexo'] == 'm':
+            arquivo = open('Aulas/Aula18/lista_homem.txt','a')
+            arquivo.write(f"{linha['codigo']};{linha['nome']};{linha['sexo']};{linha['idade']}\n")
+            arquivo.close()
+
+def entrada():       
+    cod = input('Digite o codigo: ')
+    arquivo = open('Aulas/Aula18/lista_mulher.txt','r')
+    for linha in arquivo:
+        lista_linha = linha.split(';')
+        if lista_linha[0] == cod:
+            if lista_linha[3] < '18':
+                print("Entrada Proibida!")
+                break
+            else:
+                print(f'{lista_linha[1]} - R$ 15,00')
+                break  
+    arquivo.close()
+
+    arquivo = open('Aulas/Aula18/lista_homem.txt','r')
+    for linha in arquivo:
+        lista_linha = linha.split(';')
+        if lista_linha[0] == cod:
+            if lista_linha[3] < '18':
+                print("Entrada Proibida!")
+                break
+            else:
+                print(f'{lista_linha[1]} - R$ 35,00')
+                break
+    arquivo.close()
+    return cod
+
+convidados = dicionario()
+# separar_sexo()
+codigo = '1'
+while codigo > '0':
+    codigo = entrada()
+    
