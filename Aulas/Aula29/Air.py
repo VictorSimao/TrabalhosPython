ffa5deb4deaf3b9badef01ef4fbe9eaed22e65da
unknown <900165@proway.treina>
2020-01-07 11:03:35 -0300

    Aula 29 Revisada

--- Aulas/Aula29/Air.py
+++ Aulas/Aula29/Air.py
@@ -1,11 +1,38 @@
-def listar_terminal(terminal):
-    for pessoa in terminal:
-        print(pessoa)
+class airlines:
+    '''
+    Classe que possibilita listar as pessoas em cada
+    lugar (carro, avião e terminal) e transferi-los.
+    '''
 
-def listar_aviao(aviao):
-    for pessoa in aviao:
-        print(pessoa)
+    def listar_terminal(self, terminal):
+        '''
+        Lista todas as pessoas que estão no terminal.
+        '''
+        for i in range(len(terminal)):
+            print(terminal[i])
 
-def listar_carro(carro):
-    for pessoa in carro:
-        print(pessoa)
+    def listar_aviao(self, aviao):
+        '''
+        Lista todas as pessoas que estão no avião.
+        '''
+        for pessoa in aviao:
+            print(pessoa)
+
+    def listar_carro(self, carro):
+        '''
+        Lista todas as pessoas que estão no carro.
+        '''
+        if not carro:
+            print('\nCarro: Vazio')
+        else:
+            try:
+                print(f'\nCarro: {carro[0]} e {carro[1]}')
+            except IndexError:
+                print(f'\nCarro: {carro[0]}')
+
+    def transfer(self, recebe, exclue):
+        '''
+        Realiza a tranferência dos passageiros.
+        '''
+        recebe.append(exclue[0])
+        exclue.pop(0)
--- Aulas/Aula29/exercicio1_up.py
+++ Aulas/Aula29/exercicio1_up.py
@@ -1,43 +1,44 @@
-def transfer(recebe,exclue):
-    recebe.append(exclue[0])
-    exclue.pop(0)
+from Air import airlines
+air = airlines()
 
 terminal = ['piloto','oficial_1','oficial_2','chefe','comissario_1','comissario_2','policial','prisioneiro']
 carro = []
 aviao = [] 
 
 while not not terminal:
-    print('########## TERMINAL #########')
-    print(f'Terminal: {terminal}')
-    print(f'Carro: {carro}')
+    print('\n########## TERMINAL #########')
+    air.listar_terminal(terminal)
+    air.listar_carro(carro)
     if not carro:
-        transfer(carro,terminal)
-    print('########## INDO PARA O AVIAO ##########')
+        air.transfer(carro,terminal)
+    print('\n########## INDO PARA O AVIAO ##########')
     if terminal[0] == 'policial':
         terminal.append(carro[0])    
         carro.clear()
-        transfer(carro,terminal)
-        transfer(carro,terminal)
+        air.transfer(carro,terminal)
+        air.transfer(carro,terminal)
     else:
-        transfer(carro,terminal)
+        air.transfer(carro,terminal)
     print(f'Dirigindo: {carro[0]} Passageiro: {carro[1]}')
-    print('########## AVIAO ##########')
+    print('\n########## AVIAO ##########')
     if carro[0] == 'policial':
         carro.append('piloto')
         aviao.pop(2)
-        transfer(aviao,carro)
-        transfer(aviao,carro)
+        air.transfer(aviao,carro)
+        air.transfer(aviao,carro)
     elif carro[1] == 'chefe':
-        transfer(aviao,carro)
+        air.transfer(aviao,carro)
     else:
         aviao.append(carro[-1])
         carro.pop(-1)
-    print(f'Aviao: {aviao}')
+    air.listar_aviao(aviao)
     if not not terminal:
-        print('########## INDO PARA O TERMINAL ##########')
-        print(f'Dirigindo: {carro}')
+        print('\n########## INDO PARA O TERMINAL ##########')
+        print(f'Dirigindo: {carro[0]}')
     else:
+        print(f'\nPor último, {carro[0]} embarca no avião.')
         aviao.append(carro[-1])
         carro.clear()
-        print(f'Aviao: {aviao}')
-        print('EMBARQUE FINALIZADO')
+        print('\n########## AVIAO ##########')
+        air.listar_aviao(aviao)
+        print('\nEMBARQUE FINALIZADO!')
