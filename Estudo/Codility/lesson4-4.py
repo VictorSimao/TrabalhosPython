79f40081769e9de568837432a7f9e98eb43deaf5
VictorSimao <victordiogosimao@gmail.com>
2020-03-02 11:49:55 -0300

    Codility Lesson 4 quase

--- Estudo/Codility/lesson4-4.py
+++ Estudo/Codility/lesson4-4.py
@@ -1,6 +1,21 @@
 
 def solution(A):
-    lista = set(A)
-    if len(lista) == max(A):
+    set(A)
+    print(A)
+    if 0 in A:
+        A.remove(0)
+    if len(A) == max(A):
         return 1
     return 0
+
+A = [0,1,5,6,8,4,3,2,7]
+assert solution(A) == 1
+
+A = [1,2]
+assert solution(A) == 1
+
+A = [1,3]
+assert solution(A) == 0
+
+A = [1,1]
+assert solution(A) == 0
