c8f85b3b5897ccc2e067c24f00ac13b9c9591608
victorsimao <victordiogosimao@gmail.com>
2020-03-02 22:30:30 -0300

    Codility Lessons 1, 2, 3, 4 ok

--- Estudo/Codility/lesson3-3.py
+++ Estudo/Codility/lesson3-3.py
@@ -4,20 +4,35 @@
 # A[3] = 4
 # A[4] = 3
 
-def solution(A):
-    left = A[0]
-    right = sum(A[1::])
-    diff = abs(left - right)
-
-    for p in range(1, len(A)):
-        ldiff = abs(left - right)
-        if ldiff < diff:
-            diff = ldiff
-        left += A[p]
-        right -= A[p]
-
-    return diff
+# def solution(A):
+#     left = A[0]
+#     right = sum(A[1::])
+#     diff = abs(left - right)
+#
+#     for p in range(1, len(A)):
+#         ldiff = abs(left - right)
+#         if ldiff < diff:
+#             diff = ldiff
+#         left += A[p]
+#         right -= A[p]
+#
+#     return diff
 
 A = [3,1,2,4,3]
 
 print(solution(A))
+
+
+def solution(A):
+    resto = []
+    left = 0
+    right = sum(A)
+
+    for i in A:
+        left += i
+        right -= i
+        if left >= right:
+            resto.append(left - right)
+        elif right > left:
+            resto.append(right - left)
+    return min(resto)
